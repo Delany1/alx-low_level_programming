ad8586f29cccceaabc28ad97715c76543c922da9
Faith <akinolaadelani@gmail.com>
2022-09-13 10:18:07 -0700

    program that prints putchar followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,10 @@
  */
 
 int main(void)
+
 {
 	_putchar('_');
-	_putchar('P');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
@@ -20,4 +21,3 @@ int main(void)
 
 	return (0);
 }
-
