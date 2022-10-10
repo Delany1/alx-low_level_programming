da9dca4a3301fbd401717fedf360e1ccf12393a9
Faith <akinolaadelani@gmail.com>
2022-10-10 05:06:20 -0700

    3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,5 @@
-#ifndef ABS_X
-#define ABS_X
-#define ABS(X) ((X < 0) ? (X * -1) : (X))
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+
+#define ABS(X) ((X < (0)) ? ((X) * (-1)) : (X))
 #endif
